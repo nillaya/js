4265f856fb43aa2f0f7919ae8abacb1186e41dbe
nillaya <5920610002@psu.ac.th>
2020-01-07 10:37:27 +0700

    ลดจำนวนรอบ

--- setInterval.js
+++ setInterval.js
@@ -2,7 +2,7 @@ var i = 0
 var x = setInterval(function() { //ใช้ตัวแปรเพื่อไปใช้ในการยกเลิก
     console.log('i =', i)
     i++
-    if (i === 5 ){
+    if (i === 3 ){
         clearInterval(x)   
     }
 }, 1000)
